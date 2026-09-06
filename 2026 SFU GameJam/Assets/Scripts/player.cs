cf42bb0ab6394945565577a81d5c961fdb501d1f
baguette <139591238+ilikebaguettes@users.noreply.github.com>
2026-09-06 03:34:01 -0700

    remove progress thing for build to work

--- 2026 SFU GameJam/Assets/Scripts/player.cs
+++ 2026 SFU GameJam/Assets/Scripts/player.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public class player : MonoBehaviour
 {
